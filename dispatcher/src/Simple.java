b50e553b8111b2314adcc9cbcfef1da4504c0d0d
Avalon-PC <Avalon-PC@Avalon-PC-PC>
2015-12-22 16:43:35 +0530

    seventh commit add line in simple

--- dispatcher/src/Simple.java
+++ dispatcher/src/Simple.java
@@ -1,10 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-
 public class Simple extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -12,19 +8,19 @@ public class Simple extends HttpServlet {
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
-		String p=request.getParameter("userPass");
-		if(p.equals("servlet")){
-			RequestDispatcher rd=request.getRequestDispatcher("welcome");
+
+		String p = request.getParameter("userPass");
+		String p = request.getParameter("userPass");
+		if (p.equals("servlet")) {
+			RequestDispatcher rd = request.getRequestDispatcher("welcome");
 			rd.forward(request, response);
-			
-		}
-		else{
+
+		} else {
 			out.print("Sorry username or password error!");
-			RequestDispatcher rd=request.getRequestDispatcher("login.html");
+			RequestDispatcher rd = request.getRequestDispatcher("login.html");
 			rd.include(request, response);
 		}
-		
+
 	}
 
 }
