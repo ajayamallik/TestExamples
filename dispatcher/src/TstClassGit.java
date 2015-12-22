acc38441ace81578857292cef5adf7baa9539b09
Avalon-PC <Avalon-PC@Avalon-PC-PC>
2015-12-22 17:05:12 +0530

    new classssss

---
+++ dispatcher/src/TstClassGit.java
@@ -0,0 +1,12 @@
+public class TstClassGit {
+
+	public TstClassGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fsdfsdf");
+	}
+
+}
