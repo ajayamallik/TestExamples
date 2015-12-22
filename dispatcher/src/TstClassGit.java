38e06d00222d05071649c2c7e100c4c7a7db4541
Avalon-PC <Avalon-PC@Avalon-PC-PC>
2015-12-22 17:06:35 +0530

    New 3 lines are added

--- dispatcher/src/TstClassGit.java
+++ dispatcher/src/TstClassGit.java
@@ -7,6 +7,9 @@ public class TstClassGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("fsdfsdf");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 	}
 
 }
